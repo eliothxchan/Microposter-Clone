707d78b1cd54c46b26a23ce46d8b579d06f53863
Eliot Chan <eliot.chan@hotmail.ca>
2015-02-26 19:46:50 +0000

    Set root endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  root 'static_page/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
