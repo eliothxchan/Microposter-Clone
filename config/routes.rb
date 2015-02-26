9942f52f7d089cad1344441547d7faf7124410d5
Eliot Chan <eliot.chan@hotmail.ca>
2015-02-26 20:13:10 +0000

    Refactor and add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
+  get 'static_pages/about'
   get 'static_pages/help'
   
-  root 'static_page/home'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
